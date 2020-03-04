f5bea2ed930b2a2ba135aa9acdc45454fff8e5c7
Ron Hughes <ronald.hughes1982@gmail.com>
2020-03-04 17:33:31 -0500

    added bulma

--- my-app/src/components/display.js
+++ my-app/src/components/display.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getName } from '../store/actions'
+import '../sass/mystyles.scss'
 
+
+// de-structure in actions and state key variables
 export const Display = ({ getName, name, img }) => {
 
     return (
         <>
-      Name: { name }<br />
-    {img ? <img src={ img } width="325px" height="325px" /> : <h1>image goes here</h1>}   <br />
+      <div className="title">Name:</div> { name }<br />
+    {img ? <img src={ img } style={{ width: "325px", height: "325px" }} /> : <h1>image goes here</h1>}   <br />
       <button onClick={ getName }> Click me</button>
         </>
     )
